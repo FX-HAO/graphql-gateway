0471f4e22474ff2566aed7a83cea261bd0f661ec
gengjiawen <technicalcute@gmail.com>
2017-10-27 12:44:47 +0800

    update mock server

--- mock/index.js
+++ mock/index.js
@@ -7,11 +7,17 @@ import { schema } from './schema';
 const app = new koa();
 const router = new koaRouter();
 
-app.use((ctx, next) => {
-  if (ctx.headers["authorization"] === undefined) {
-    ctx.throw(401, "invalid grant");
+app.use(async (ctx, next) => {
+  await next()
+  if (ctx.request.body !== undefined) {
+    console.log(ctx.request.body);
+    const v = ctx.request.body.variables;
+    if (v["name"] !== undefined) {
+      if (v["name"] === "401") {
+        ctx.throw(401, "invalid grant");
+      }
+    }
   }
-  next()
 });
 
 // koaBody is needed just for POST.
@@ -21,6 +27,8 @@ router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
 
 const PORT = 4000;
 
+app.use(router.routes());
+app.use(router.allowedMethods());
 app.listen(PORT, () =>
   console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
 );
